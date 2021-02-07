b8a3d0ece5365221788434d54771f217704273cc
hajar <hajarlablaoui042@gmail.com>
2021-02-07 21:20:38 +0100

    DB Creation + price

--- dbCreation.py
+++ dbCreation.py
@@ -1,106 +1,72 @@
-import sqlite3
 import mysql.connector
 import datetime
-dbName = "carsPlates.db"
 
-#TableSchema = """
-#drop table if exists car;
-#CREATE table car(
-#id integer primary key autoincrement,
-#plate text,
-#dateEntree date,
-#dateSortie date
-#);
-#"""
-
-'''
-#Connection
-conn=sqlite3.connect(dbName)
-curs=conn.cursor()
-
-#curs.execute("INSERT INTO car VALUES (null ,'32-23M','2006-01-05','2006-01-06')")
-curs.execute("SELECT * FROM car")
-print(curs.fetchone())
-conn.commit()
-#Tables
-#sqlite3.complete_statement(TableSchema)
-#curs.executescript(TableSchema)
-
-curs.close()
-conn.close()
-'''
-
-mydb=mysql.connector.connect(
-    host = "localhost",
-    username = "root",
-    passwd = "",
-    database = "carsPlates",
+mydb = mysql.connector.connect(
+    host="localhost",
+    username="root",
+    passwd="",
+    database="carsPlates",
 )
 
-mycursor=mydb.cursor()
+mycursor = mydb.cursor()
 
-#mycursor.execute("CREATE database carsPlates")
-#mycursor.execute("DROP table if exists facture")
-#mycursor.execute("Create table car(id integer primary key AUTO_INCREMENT,plate varchar(20),dateEntree date, dateSortie date)")
+# mycursor.execute("CREATE database carsPlates")
+# mycursor.execute("DROP table if exists facture")
+# mycursor.execute("Create table car(id integer primary key AUTO_INCREMENT,plate varchar(20),dateEntree date, dateSortie date)")
 
-#mycursor.execute("Create table park(id integer primary key AUTO_INCREMENT, plate varchar(20),dateEntree datetime, dateSortie datetime default null)")
+# mycursor.execute("Create table park(id integer primary key AUTO_INCREMENT, plate varchar(20),dateEntree datetime, dateSortie datetime default null)")
 
-#sqlform = "insert into park(plate,dateEntree,dateSortie) values (%s,%s,%s)"
+# sqlform = "insert into park(plate,dateEntree,dateSortie) values (%s,%s,%s)"
 
-#ars = [("34-ABC","2006-01-05","2006-01-05"),("78648AABB","2006-01-05","2006-01-05"),("OIAHDKX SJ","2006-01-05","2006-01-05")]
+# ars = [("34-ABC","2006-01-05","2006-01-05"),("78648AABB","2006-01-05","2006-01-05"),("OIAHDKX SJ","2006-01-05","2006-01-05")]
 
-#mycursor.executemany(sqlform,cars)
+# mycursor.executemany(sqlform,cars)
 
-#mycursor.execute("Create table facture(id integer primary key AUTO_INCREMENT,  parkId int,FOREIGN KEY (parkId) REFERENCES park(id),price double,datePayment datetime default null)")
+# mycursor.execute("Create table facture(id integer primary key AUTO_INCREMENT,  parkId int,FOREIGN KEY (parkId) REFERENCES park(id),price double,datePayment datetime default null)")
 
 
 mydb.commit()
+
+
 def plateToDB(plate):
-    #check if exists row with plate & dateSortie null
-        #update row with new now date
-    #else
-        #insert new row with dateEntre Now
+    # check if exists row with plate & dateSortie null
+    # update row with new now date
+    # else
+    # insert new row with dateEntre Now
 
     sql = "SELECT * FROM park WHERE plate = %s and dateSortie is NULL"
-    pl = (plate, )
+    pl = (plate,)
 
     mycursor.execute(sql, pl)
 
     myresult = mycursor.fetchall()
     print(myresult)
-    if len(myresult)>0:
-        sql="UPDATE park SET dateSortie = %s where id = %s"
-        pl = (datetime.datetime.now(),myresult[0][0])
+    if len(myresult) > 0:
+        sql = "UPDATE park SET dateSortie = %s where id = %s"
+        pl = (datetime.datetime.now(), myresult[0][0])
         mycursor.execute(sql, pl)
         mydb.commit()
         sql = "SELECT * FROM park WHERE id = %s "
         pl = (myresult[0][0],)
         mycursor.execute(sql, pl)
         myresult1 = mycursor.fetchall()
-        parcPrice(myresult1[0][0],myresult1[0][1],myresult1[0][2])
-    else :
+        parcPrice(myresult1[0][0], myresult1[0][1], myresult1[0][2])
+    else:
         sqlform = "insert into park(plate,dateEntree) values (%s,%s)"
-        pl = (plate,datetime.datetime.now())
+        pl = (plate, datetime.datetime.now())
         mycursor.execute(sqlform, pl)
         mydb.commit()
 
-def parcPrice(id,dateEntree,dateSortie) :
+
+def parcPrice(id, dateEntree, dateSortie):
     heureprix = 10
     FMT = '%Y/%d/%m %H:%M:%S'
     print(dateEntree)
     tdelta = datetime.datetime.strptime(dateSortie, FMT) - datetime.datetime.strptime(dateEntree, FMT)
     print(tdelta)
     price = 0
-    if price ==0:
+    if price == 0:
         price = 10
     sqlform = "insert into facture(parkId,price) values (%s,%s)"
     pl = (id, price)
     mycursor.execute(sqlform, pl)
-
-
-
-
-
-
-
-
