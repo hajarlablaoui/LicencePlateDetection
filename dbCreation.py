55697dcf65aca0caad7d3025d0fc47f83f93207e
hajar <hajarlablaoui042@gmail.com>
2021-02-09 12:33:03 +0100

    final commit

--- dbCreation.py
+++ dbCreation.py
@@ -1,7 +1,7 @@
 import mysql.connector
 import datetime
 
-
+#connection à la base de données
 mydb = mysql.connector.connect(
     host="localhost",
     username="root",
@@ -13,23 +13,21 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 #mycursor.execute("CREATE database carsPlates")
-#mycursor.execute("DROP table if exists facture")
 #mycursor.execute("Create table park(id integer primary key AUTO_INCREMENT, plate varchar(20),dateEntree datetime, dateSortie datetime default null)")
 #mycursor.execute("Create table facture(id integer primary key AUTO_INCREMENT,  parkId int,FOREIGN KEY (parkId) REFERENCES park(id),price double,datePayment datetime default now())")
+
 mydb.commit()
 
 
+#insertion des plaques dans la base de données
 def plateToDB(plate):
-    # check if exists row with plate & dateSortie null
-    # update row with new now date
-    # else
-    # insert new row with dateEntre Now
-
+    # vérifier si il la plaque existe avec une date de sortie null
     sql = "SELECT * FROM park WHERE plate = %s and dateSortie is NULL"
     pl = (plate,)
     mycursor.execute(sql, pl)
     myresult = mycursor.fetchall()
-    print(myresult)
+
+   #mise à jour de la ligne avec une date de sortie
     if len(myresult) > 0:
         sql = "UPDATE park SET dateSortie = %s where id = %s"
         pl = (datetime.datetime.now(), myresult[0][0])
@@ -41,6 +39,7 @@ def plateToDB(plate):
         myresult1 = mycursor.fetchall()
         parcPrice(myresult1[0][0], myresult1[0][2], myresult1[0][3])
 
+    # inserer une nouvelle ligne avec une date d'entrée
     else:
         sqlform = "insert into park(plate,dateEntree) values (%s,%s)"
         pl = (plate, datetime.datetime.now())
@@ -48,12 +47,18 @@ def plateToDB(plate):
         mydb.commit()
 
 
+#calcul du prix
 def parcPrice(id, dateEntree, dateSortie):
-    prixheure = 10 #dh
-    time=(dateSortie-dateEntree).total_seconds()
+    prixheure = 10 #dh #prix par heure
+
+    #calcul du temps dans le parking
+    time = (dateSortie-dateEntree).total_seconds()
+    #calcul du prix
     price = (time / 3600) * prixheure
+    #définition d'un prix minimum
     if price < 10:
         price = 10
+    #insertion de la facture dans la base de données
     sqlform = "insert into facture(parkId,price) values (%s,%s)"
     pl = (id, price)
     mycursor.execute(sqlform, pl)
--- main.py
+++ main.py
@@ -4,22 +4,31 @@ import numpy as np
 import pytesseract
 from dbCreation import plateToDB
 import random
-
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 images=['images/2088.jpeg','images/audi.jpg','images/peugeot.jpg','images/skoda.jpeg','images/volvo.jpg']
-img = cv2.imread(random.choice(images),cv2.IMREAD_COLOR)
-img = cv2.resize(img, (600,400) )
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
+#--------------------------------------------------------------------------------------#
+img = cv2.imread(random.choice(images),cv2.IMREAD_COLOR)
+
+#redimensionnement + bg_gris
+img = cv2.resize(img, (600,400) )
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#supression des détails indésirables (flou)
+#gray = cv2.bilateralFilter (source_image, diamètre du pixel, sigmaColor, sigmaSpace)
 gray = cv2.bilateralFilter(gray, 13, 15, 15)
 
+#détection des bords
+#edged = cv2.Canny (source_image, thresholdMin, thresholdMax)
 edged = cv2.Canny(gray, 30, 200)
 
+#recherche des contours + tri du plus grand au plus petit
 contours = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 contours = imutils.grab_contours(contours)
 contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
 screenCnt = None
 
+#vérification (4 contours + fermé)
 for c in contours:
     peri = cv2.arcLength(c, True)
     approx = cv2.approxPolyDP(c, 0.018 * peri, True)
@@ -34,26 +43,32 @@ if screenCnt is None:
 else:
      detected = 1
 
+#dessiner un contour en rouge
 if detected == 1:
     cv2.drawContours(img, [screenCnt], -1, (0, 0, 255), 3)
 
+#masquer l'image sauf "plate"
 mask = np.zeros(gray.shape,np.uint8)
 new_image = cv2.drawContours(mask,[screenCnt],0,255,-1,)
 new_image = cv2.bitwise_and(img,img,mask=mask)
 
+#recadrage de l'image
 (x, y) = np.where(mask == 255)
 (topx, topy) = (np.min(x), np.min(y))
 (bottomx, bottomy) = (np.max(x), np.max(y))
 Cropped = gray[topx:bottomx+1, topy:bottomy+1]
 
+#conversion to string
 text = pytesseract.image_to_string(Cropped, config='--psm 11')
 plateToDB(text)
 
-print("programming_fever's License Plate Recognition\n")
-print("Detected license plate Number is:",text)
+print("--------------------------------------------")
+print("La plaque détectée est :",text)
+print("--------------------------------------------")
 
 img = cv2.resize(img,(500,300))
 Cropped = cv2.resize(Cropped,(400,200))
+
 cv2.imshow('car',img)
 cv2.imshow('Cropped',Cropped)
 
